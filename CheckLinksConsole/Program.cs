bef1782e32981ef33f1f13cb7d1a082d0690c2ef
Frank Cheung <fcuk112@gmail.com>
2018-08-27 13:22:25 +0100

    add hangfire dashboard.

--- CheckLinksConsole/Program.cs
+++ CheckLinksConsole/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using Hangfire;
 using Hangfire.MemoryStorage;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 
 namespace CheckLinksConsole
@@ -14,15 +16,22 @@ namespace CheckLinksConsole
             var config = new Config(args);
             Logs.Init(config.ConfigurationRoot);
 
-            GlobalConfiguration.Configuration.UseMemoryStorage();
-            RecurringJob.AddOrUpdate<CheckLinkJob>("check-link", j => j.Execute(config.Site, config.Output), Cron.Minutely);
-            RecurringJob.Trigger("check-link");
+            var host = CreateWebHostBuilder(args).Build();
 
-            using (var server = new BackgroundJobServer())
-            {
-                Console.WriteLine("Hangfire Server started.  Press any key to exit...");
-                Console.ReadKey();
-            }
+            // RecurringJob.AddOrUpdate<CheckLinkJob>("check-link", j => j.Execute(config.Site, config.Output), Cron.Minutely);
+            // RecurringJob.Trigger("check-link");
+            RecurringJob.AddOrUpdate(() => Console.Write("Simple!"), Cron.Minutely);
+
+            host.Run();
+
+            // using (var server = new BackgroundJobServer())
+            // {
+            //     Console.WriteLine("Hangfire Server started.");
+            //     host.Run();
+            // }
         }
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>();
     }
 }
